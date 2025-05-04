ffd42c8ea935dc8170cca86ed8d5f969f94a8ff8
jmusrose <1240465930@qq.com>
2025-05-04 16:22:22 +0800

    1

---
+++ joeynmt/__main__.py
@@ -0,0 +1,24 @@
+import argparse
+from joeynmt.config import load_config
+from pathlib import Path
+
+
+
+
+def main():
+    ap = argparse.ArgumentParser("joeynmt")
+    ap.add_argument("mode", choices=["train", "test", "translate"], help = "选择训练模式")
+    ap.add_argument("config_path", metavar="config-path", type=str, help="配置文件路径")
+    ap.add_argument("-o", "--output-path", type=str, help="翻译所保存的路径")
+    ap.add_argument("-a", "--save-attention", action="store_true", help="注意力机制可视化")
+    ap.add_argument("-s", "--save-scores", action="store_true", help="保存分数")
+    ap.add_argument("-t", "--skip-test", action="store_true", help="训练之后跳过测试阶段")
+    ap.add_argument("-d", "--use-ddp", action="store_true", help="Invoke DDP environment")
+
+    args = ap.parse_args()
+    cfg = load_config(Path(args.config_path))
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ joeynmt/config.py
@@ -0,0 +1,27 @@
+import torch
+import yaml
+from pathlib import Path
+from typing import Dict
+
+def _check_path(path:str, allow_empty:bool=True) -> Path:
+    """检查路径是否存在"""
+    if path is not None:
+        path = Path(path).absolute()
+        if not allow_empty:
+            assert path.exists(), f"{path}没有找到"
+    return path
+
+
+def load_config(cfg_files: str = "configs/default.yaml") -> Dict:
+    """
+    读取和解析Yaml文件
+    :param cfg_files:YAML路径
+    :return:config字典
+    """
+    cfg_file = _check_path(cfg_files, allow_empty=False)
+    with cfg_file.open("r", encoding="utf-8") as ymlfile:
+        cfg = yaml.safe_load(ymlfile)
+
+    if "model_dir" not in cfg:
+        cfg["model_dir"] = cfg["training"]["model_dir"]
+    return cfg
